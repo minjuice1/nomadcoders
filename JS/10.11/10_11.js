30109481b8ccba607b3a2c743b37c19eec8005b8
minjuice1 <minjuice93@gmail.com>
2021-10-11 23:57:00 +0900

    button에 delete 기능 추가.

--- JS/10.11/10_11.js
+++ JS/10.11/10_11.js
@@ -3,14 +3,22 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  // todo-list에서 어떤 li를 클릭했는지 알고 싶을 때
+  // console.log(event.target.parentElement.innerText);
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
+  span.innerText = newTodo;
   const button = document.createElement("button");
   button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = newTodo;
   toDoList.appendChild(li);
 }
 
