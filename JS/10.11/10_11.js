291feb700851d2a7de80a0786833798dfef2e538
minjuice1 <minjuice93@gmail.com>
2021-10-11 21:43:20 +0900

    newTodo를 인자로 받는 paintTodo function 생성.

--- JS/10.11/10_11.js
+++ JS/10.11/10_11.js
@@ -3,12 +3,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function paintToDo(newTodo) {
+  console.log("i will paint", newTodo);
+}
+
 function handleToDoSubmit(event) {
   // preventDefault()를 추가하면 새로고침해도 event가 작동하지 않음.
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
-  console.log(newTodo, toDoInput.value);
+  paintToDo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
