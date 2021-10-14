c26af0ae4f92cfd34691706db8c4234cd1c7b840
minjuice1 <minjuice93@gmail.com>
2021-10-14 14:14:34 +0900

    weather open API를 이용해서 날씨 정보를 갖고옴, fetch와 비동기를 사용하여, 화면단에 나타냄

--- JS/10.13/10_13.js
+++ JS/10.13/10_13.js
@@ -1,8 +1,18 @@
+const API_KEY = "225ceae7b9ba88e8a410f328c51df6e8"
+
 function onGeoSuccess(position) {
   const lat = position.coords.latitude; 
   const lng = position.coords.longitude; 
-  console.log("You live in", lat, lng);
-  // console.log(position)
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  // fetch(url); 찍고 network 확인해보면 json 형태의 data 확인할 수 있음
+  // promise (비동기 작업) 이기에, then()을 하고 reponse, data 등의 작업을 해야 한다.
+  fetch(url).then(response => response.json()).then(data => {
+    const city = document.querySelector("#weather span:first-child");
+    const weather = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });
 }
 
 function onGeoError() {
